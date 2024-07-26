6538c02ce3a233b6f0c30f4cb003bd636a7568d7
DaCatInDaHat <volodymyrkuznetsov87@gmail.com>
2024-07-27 01:22:39 +0300

    One more fetch url change

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -17,7 +17,7 @@ export const Create = () => {
 
     const handleSubmit = e => {
         e.preventDefault()
-        fetch('https://workshop-mate-o7v7fvomj-valdimars-projects.vercel.app/tasks', {
+        fetch('https://workshop-mate.vercel.app/tasks', {
             method: 'POST',
             headers: { "Content-type": "application/json" },
             body: JSON.stringify({ title, description, status })
