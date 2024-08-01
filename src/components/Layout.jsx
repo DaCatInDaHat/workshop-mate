f183f8a3c0baa33301772aec94b571528eb243de
DaCatInDaHat <volodymyrkuznetsov87@gmail.com>
2024-08-01 17:28:46 +0300

    add responsive side panel

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Box from '@mui/material/Box'
 import Drawer from '@mui/material/Drawer'
 import AppBar from '@mui/material/AppBar'
@@ -8,6 +8,8 @@ import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemText from '@mui/material/ListItemText'
 import { ListItemButton, ListItemIcon } from '@mui/material'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
 import BallotOutlinedIcon from '@mui/icons-material/BallotOutlined'
 import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined'
 import WarehouseOutlinedIcon from '@mui/icons-material/WarehouseOutlined'
@@ -33,40 +35,88 @@ const menuItems = [
 ]
 
 export const Layout = ({ children }) => {
+    const [mobileOpen, setMobileOpen] = useState(false)
+    const [isClosing, setIsClosing] = useState(false)
+
+    const handleDrawerClose = () => {
+        setIsClosing(true)
+        setMobileOpen(false)
+    }
+
+    const handleDrawerTransitionEnd = () => {
+        setIsClosing(false)
+    }
+
+    const handleDrawerToggle = () => {
+        if (!isClosing) {
+            setMobileOpen(!mobileOpen)
+        }
+    }
+
+    const drawer = (
+        <List>
+            {menuItems.map(item => (
+                <ListItem key={item.text}>
+                    <ListItemButton component="a" href={item.path}>
+                        <ListItemIcon>{item.icon}</ListItemIcon>
+                        <ListItemText primary={item.text} />
+                    </ListItemButton>
+                </ListItem>
+            ))}
+        </List>
+    )
+
     return (
         <Box display='flex'>
             <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
                 <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{ mr: 2, display: { sm: 'none' } }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
                     <Typography variant="h6" noWrap component="div">
                         {document.title}
                     </Typography>
                 </Toolbar>
             </AppBar>
-            <Drawer
-                variant='permanent'
-                anchor='left'
-                sx={{
-                    width: drawerWidth,
-                    flexShrink: 0,
-                    '& .MuiDrawer-paper': {
-                        width: drawerWidth,
-                        boxSizing: 'border-box',
-                    }
-                }}>
-                <Toolbar />
-                <Box sx={{ overflow: 'auto' }}>
-                    <List>
-                        {menuItems.map(item => (
-                            <ListItem key={item.text}>
-                                <ListItemButton component="a" href={item.path}>
-                                    <ListItemIcon>{item.icon}</ListItemIcon>
-                                    <ListItemText primary={item.text} />
-                                </ListItemButton>
-                            </ListItem>
-                        ))}
-                    </List>
-                </Box>
-            </Drawer>
+            <Box
+                component="nav"
+                sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+            >
+                <Drawer
+                    variant="temporary"
+                    open={mobileOpen}
+                    onTransitionEnd={handleDrawerTransitionEnd}
+                    onClose={handleDrawerClose}
+                    ModalProps={{
+                        keepMounted: true,
+                    }}
+                    sx={{
+                        display: { xs: 'block', sm: 'none' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                >
+                    <Box sx={{ mt: 7 }}>{drawer}</Box>
+                </Drawer>
+                <Drawer
+                    variant='permanent'
+                    sx={{
+                        display: { xs: 'none', sm: 'block' },
+                        '& .MuiDrawer-paper': {
+                            width: drawerWidth,
+                            boxSizing: 'border-box',
+                        }
+                    }}
+                    open>
+                    <Toolbar />
+                    {drawer}
+                </Drawer>
+            </Box>
             <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
                 <Toolbar />
                 {children}
